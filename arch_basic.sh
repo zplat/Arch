c8fcb12d13683350dd05226342b00ba1c6264337
zplat <5zero.6cool@gmail.com>
2022-06-22 21:45:06 +0100

    Layout

--- arch_basic.sh
+++ arch_basic.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
-# Key variables
+#-------------------------------------------------------------------------------
+#                                                                  Key variables
 HOST_NAME=""
 ROOT_PASSWD=""
 USER=""
 USER_PASSWD=""
 SETUP_URL="https://raw.githubusercontent.com/zplat/Arch/main/basic_home_setup.sh"
 LOCAL_RESPOSITORY=".local/repositories/Arch"
-
-# set time
+#-------------------------------------------------------------------------------
+#                                                                  set time
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc --localtime
-
-# set language locale. Well over the top! 
+#-------------------------------------------------------------------------------
+#                                                                  set language locale.  
 echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -21,28 +22,28 @@ echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen
 echo "it_IT.UTF-8 UTF-8" >> /etc/locale.gen
 echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
 echo "ko_KR.UTF-8 UTF-8" >> /etc/locale.gen
-echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
-
-# Update locale
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen  #Well over the top!
+#-------------------------------------------------------------------------------
+#                                                                  Update locale
 locale-gen
-
-# Configure locale.conf.
+#-------------------------------------------------------------------------------
+#                                                                  Configure locale.conf.
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-# Set terminal fonts
+#-------------------------------------------------------------------------------
+#                                                                  Set terminal fonts
 echo "FONT=ter-132n" >> /etc/vconsole.conf
 echo "FONT_MAP=8859-1" >> /etc/vconsole.conf
-
-# Network
+#-------------------------------------------------------------------------------
+#                                                                  Network
 echo "$HOST_NAME" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1  ${HOST_NAME}.localdomain $HOST_NAME" >> /etc/hosts
-
-# set password
+#-------------------------------------------------------------------------------
+#                                                                  set root password
 echo "root:${ROOT_PASSWD}" | chpasswd
-
-# install packages
+#-------------------------------------------------------------------------------
+#                                                                  install packages
 pacman -S --noconfirm grub efibootmgr reflector
 pacman -S --noconfirm networkmanager network-manager-applet dialog wpa_supplicant
 pacman -S --noconfirm base-devel linux-headers 
@@ -50,26 +51,25 @@ pacman -S --noconfirm xdg-user-dirs xdg-utils rustup
 pacman -S --noconfirm zsh zsh-completions
 pacman -S --noconfirm terminus-font
 pacman -S --noconfirm dosfstools os-prober udiskie ntfs-3g openssl
-
 pacman -S --noconfirm xf86-video-amdgpu amd-ucode
-
-# change shell bash to zsh
+#-------------------------------------------------------------------------------
+#                                                                  change shell bash to zsh
 chsh -s /bin/zsh
-
-# Update pacman repositories. multilib.
+#-------------------------------------------------------------------------------
+#                                                                  Update pacman repositories. multilib.
 sed -i 's/^#\[multilib\]/\[multilib]/' /etc/pacman.conf
 sed -i '/^\[multilib\]/ {n;s/^#Include =  \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/nirrorlist/}' /etc/pacman.conf
-
-# setup mkinitcpio for amd graphics processor. 
+#-------------------------------------------------------------------------------
+#                                                                  setup mkinitcpio for amd graphics processor. 
 sed -i 's/MODULE.*/MODULE=\(amdgpu\)/' /etc/mkinitcpio.conf 
 mkinitcpio -p linux
-
-# Install grub
+#-------------------------------------------------------------------------------
+#                                                                  Install grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# Setup systemctl hooks 
-# set reflector defaults
+#-------------------------------------------------------------------------------
+#                                                                  Setup systemctl hooks 
+#------------------------set reflector defaults
 echo "# Set the output path where the mirrorlist will be saved (--save).
 --save /etc/pacman.d/mirrorlist
 # Select the transfer protocol (--protocol).
@@ -84,7 +84,7 @@ echo "# Set the output path where the mirrorlist will be saved (--save).
 --sort score
 " > /etc/xdg/reflector/reflector.conf
 
-# Automate package cleaning with paccache
+#------------------------Automate package cleaning with paccache
 echo "[Trigger]
 Operation = Remove
 Operation = Install
@@ -97,7 +97,7 @@ When = PostTransaction
 Exec = /usr/bin/paccache -rvk2
 " > /usr/share/libalpm/hooks/paccache.hook
 
-# list of orphan apps to remove
+#------------------------list of orphan apps to remove
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -108,7 +108,7 @@ When = PostTransaction
 Exec = /usr/bin/bash -c "/usr/bin/pacman -Qtd || /usr/bin/echo '==> no orphans found'"
 " > /usr/share/libalpm/hooks/pkgClean.hook
 
-# list of Core programs
+#------------------------list of Core programs
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -119,7 +119,7 @@ When = PostTransaction
 Exec = /bin/sh -c '/usr/bin/pacman -Qqn > /home/$USER/$LOCAL_RESPOSITORY/corepkglist.txt'
 " > /usr/share/libalpm/hooks/pkgCore.hook
 
-# list of AUR programs
+#------------------------list of AUR programs
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -130,7 +130,7 @@ When = PostTransaction
 Exec = /bin/sh -c '/usr/bin/pacman -Qqm > /home/$USER/$LOCAL_RESPOSITORY/aurpkglist.txt'
 " > /usr/share/libalpm/hooks/pkgAUR.hook
 
-# Trigger udiskie on boot
+#------------------------Trigger udiskie on boot
 echo "[Unit]
 Description = Handle automounting of usb devices
 
@@ -142,23 +142,23 @@ ExecStart = /usr/bin/udiskie
 WantedBy = multi-user.target
 " > /etc/systemd/user/udiskie.service
 
-#Systemd to start udiskie on startup
+#-------------------------------------------------------------------------------
+#                                                                  Systemd to start udiskie on startup
 chmod 755 /etc/systemd/user/udiskie.service
 
-#Update systemd to ensure dropbox starts on login
+#-------------------------------------------------------------------------------
+#                                                                  Update systemd to ensure dropbox starts on login
 systemctl enable dropbox@$USER
-
-
-# cacche
+#-------------------------------------------------------------------------------
+#                                                                  cacche
 pacman -s --noconfirm --needed ccache
 sed -i 'x;/^BUILDENV/s/!ccache/ccache/' /etc/makepkg.conf
 CORES=$(nproc)
 LOAD=$((CORES/2))
 JOBS=$((LOAD+1))
-
 sed -i "x;/^#MAKEFLAGS/s/"-j2"/-j${JOBS} -l${LOAD}/ " /etc/makepkg.conf
-
-# setup swap
+#-------------------------------------------------------------------------------
+#                                                                  setup swap
 truncate -s 0 /swap/swapfile
 chattr +c /swap/swapfile
 btrfs property set /swap/swapfile compression none
@@ -167,34 +167,44 @@ chmod 600 /swap/swapfile
 mkswap /swap/swapfile
 swapon /swap/swapfile
 
-# update fstab with swapfile
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#                                                                  update fstab with swapfile
 echo "# swapfile" >> /etc/fstab
 echo "/swap/swapfile	none	swap	defaults	0 0" >> /etc/fstab
 
-# add user
+#-------------------------------------------------------------------------------
+#                                                                  add user
 useradd -m -g users -s /bin/zsh "$user"
 echo "${user}:${user_passwd}" | chpasswd
 curl --url "$setup_url" >> "/home/$user/shell.sh"
 
-# make user an administrator
+#-------------------------------------------------------------------------------
+#                                                                  make user an administrator
 echo "$user all=(all) all" >> "/etc/sudoers.d/$user"
 
-# enable systemd services
+#-------------------------------------------------------------------------------
+#                                                                  enable systemd services
 systemctl enable networkmanager
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable paccache.timer
 systemctl enable reflector.service
 
-# os-probe
+#-------------------------------------------------------------------------------
+#                                                                  os-probe
 pacman -s --noconfirm os-prober
 echo "grub_disable_os_prober=false" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install post install script.
+#-------------------------------------------------------------------------------
+#                                                                  Install post install script.
 curl --url "$SETUP_URL" >> "/home/$USER/hell.sh"
 
-# all done 
+#-------------------------------------------------------------------------------
+#                                                                  all done 
 printf "\e[1;32mdone! type exit, umount -a and reboot.\e[0m"
 
+#-------------------------------------------------------------------------------
 # shred -uvz shell.sh
--- arch_setup.sh
+++ arch_setup.sh
@@ -1,71 +1,61 @@
 #!/usr/bin/env sh
-#Arch install with btrfs.
 
-# Identify which drives are to be used.
-# eg BOOTDRIVE="/dev/sdc1".
-BOOTDRIVE="/dev/sd"
-ROOTDRIVE="/dev/sd"
+#                           Arch install with btrfs.
 
-# Update mirrors for faster updates.
+#-----------------------------------------------------------------------------
+#                                                                Which drives 
+BOOTDRIVE="/dev/sd" # eg BOOTDRIVE="/dev/sdc1".
+ROOTDRIVE="/dev/sd" # eg BOOTDRIVE="/dev/sdc2".
+HOMEDRIVE="/dev/sd" # eg BOOTDRIVE="/dev/sdc3". Can be same as ROOTDRIVE.
+#-----------------------------------------------------------------------------
+#                                                                Update mirrors
 pacman -Syy
 pacman - reflector
 reflector -c "United Kingdom" -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
-
-# Update network time protocol. 
-timedatectl set-ntp true 
-
-# This is the next script to be used to install Arch. 
-# Sets up a basic install.
-SETUP_URL="https://raw.githubusercontent.com/zplat/ArchInstall/master/arch_basic.sh"
-
-# Format drives
-
-mkfs.fat -F32 "$BOOTDRIVE"
-fatlabel "$BOOTDRIVE" BOOT 
-# To labil the boot drive.
-mkfs.btrfs -f "$ROOTDRIVE" 
-#Use -f to force overwrite
-
-# Mount drives
-
-mount "$ROOTDRIVE" /mnt
-
-# Create the subvolumes.
-btrfs subvolume create /mnt/@
+#-----------------------------------------------------------------------------
+#                                                                network time protocol. 
+timedatectl set-ntp true  # Update network time protocol. 
+#-----------------------------------------------------------------------------
+#                                                               Next script to install Arch. 
+SETUP_URL="https://raw.githubusercontent.com/zplat/Arch/master/arch_basic.sh"
+#-----------------------------------------------------------------------------
+#                                                               Format drives
+mkfs.fat -F32 "$BOOTDRIVE" #Format Boot partition.
+fatlabel "$BOOTDRIVE" BOOT # To label the boot drive.  
+mkfs.btrfs -f "$ROOTDRIVE"  #Format Root partition. Use -f to force overwrite. 
+mkfs.btrfs -f "$HOMEDRIVE"  #Format Home partition 
+#-----------------------------------------------------------------------------
+#                                                               Mount drives
+mount "$ROOTDRIVE" /mnt             # Volume
+btrfs subvolume create /mnt/@       # Create the subvolumes.
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@var
 btrfs subvolume create /mnt/@srv
 btrfs subvolume create /mnt/@opt
 btrfs subvolume create /mnt/@tmp
 btrfs subvolume create /mnt/@swap
-
-# Unmount the drive to mount the subvolumes.
-umount /mnt
-
-# Mount subvolumes
+umount /mnt                         # Unmount the drive to mount the subvolumes.
+#-----------------------------------------------------------------------------
+#                                                               Mount subvolumes
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ "$ROOTDRIVE" /mnt
-
 mkdir /mnt/{home,boot,var,srv,opt,tmp,swap}
-
-mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home "$ROOTDRIVE" /mnt/home
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home "$HOMEDRIVE" /mnt/home
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@srv "$ROOTDRIVE" /mnt/srv
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@opt "$ROOTDRIVE" /mnt/opt
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@tmp "$ROOTDRIVE" /mnt/tmp
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@var "$ROOTDRIVE" /mnt/var
 mount -o ssd,discard,subvol=@swap "$ROOTDRIVE" /mnt/swap
-
-# Mount the boot partition.
-mount "$BOOTDRIVE" /mnt/boot
-
-# Install  initial programs (very basic)
-pacstrap /mnt base linux linux-firmware neovim git btrfs-progs
-
-# fstab
+mount "$BOOTDRIVE" /mnt/boot      # Mount the boot partition.
+#-----------------------------------------------------------------------------
+#                                                               Install  initial programs
+pacstrap /mnt base linux linux-firmware neovim git btrfs-progs #(very basic)
+#-----------------------------------------------------------------------------
+#                                                               fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-
-# Install script from git post chroot
-curl --url "$SETUP_URL" >> /mnt/shell.sh
-
-# chroot
+#-----------------------------------------------------------------------------
+#                                                               Next install script
+curl --url "$SETUP_URL" >> /mnt/shell.sh  # Install script from git post chroot
+#-----------------------------------------------------------------------------
+#                                                               chroot
 arch-chroot /mnt
