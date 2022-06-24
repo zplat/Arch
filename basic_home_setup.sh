d6866d82312631ffd3f69fb40982e42435a8136e
zplat <5zero.6cool@gmail.com>
2022-06-24 08:44:37 +0100

    Update basic_home_setup.sh
    Updated

--- basic_home_setup.sh
+++ basic_home_setup.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh 
 
 ############################################################
-# Before using be sure that the .ssh file has been set up!!
+#
+# 			Personal set up!!
+#
 ############################################################
 
-udiskie &                  #Start udiskie 
+#udiskie &                  #Start udiskie 
 xdg-user-dirs-update       #Create XDG user directories
-#--------------------------Setup pass password manager
+
+#-----------------------Setup password manager
+
+sudo pacman -S --needed pass 
 DIR="/run/media/phlight/Partition1/configure"
 cp -r "$DIR/gnupg" ~/.local/share
 cp -r "$DIR/.ssh" ~/
 git clone git@github.com:zplat/password-store.git ~/.password-store
-#-------------------------- Install dotfiles.
+
+#-------------------------- Install dotfiles
+
 MYDOTFILES="https://github.com/zplat/MyDotfiles.git"
 git clone --bare --recurse-submodules "$MYDOTFILES" "$HOME/.dotfiles"
 function config {
@@ -28,24 +35,29 @@ fi;
 config checkout -f
 config submodule update --init --recursive
 config config status.showUntrackedFiles no
+
 #--------------------------
-sudo pacman -Syy        #Update pacman package database
-rustup default nightly  # Setup rustup
+
+sudo pacman -Syy        				# Update pacman package database
+rustup default nightly  				# Setup rustup
+
 #-----------------------Install respositories
+
 mkdir ~/.local/respositories
 cd ~/.local/repositories/
-git clone https://aur.archlinux.org/paru.git            #Feature packed AUR helper
-git clone https://github.com/zplat/Arch.git             #My Arch Installation
-git clone https://github.com/sereinity/ofi-pass.git     #Is a password promptor for pass 
+git clone https://aur.archlinux.org/paru.git            # Feature packed AUR helper
+git clone https://github.com/zplat/Arch.git             # My Arch Installation
+git clone https://github.com/sereinity/ofi-pass.git     # Is a password promptor for pass 
 cd paru
 makepkg -si
 cd
 
 #-----------------------Install packages
+
 if [ -s corepkglist.txt ]
 then
         # The file is not-empty.
-        sudo pacman -S --needed - < corepkglist.txt
+        sudo pacman -S --needed - < ~/.local/repositories/corepkglist.txt
 else
         # The file is empty.
         sudo pacman -S --needed imv mpv feh sxiv
@@ -59,11 +71,12 @@ else
         sudo pacman -S --needed bemenu-wayland bemenu 
 fi
 
+#-----------------------Install AUR packages
 
 if [ -s aurpkglist.txt ]
 then
         # The file is not-empty.
-        paru  -S --needed - < aurpkglist.txt
+        paru  -S --needed - < ~/.local/repositories/aurpkglist.txt
 else
         # The file is empty.
         paru -S alacritty-git vieb-git buku-git awesome-git lynx-git 
@@ -73,5 +86,6 @@ else
 fi
 
 #----------------------Start Dropbox
+
 dropbox 
 
