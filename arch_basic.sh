9ee09647c7b6701e3525b3f65f0efc59e08fd720
zplat <5zero.6cool@gmail.com>
2022-06-24 12:12:58 +0100

    Update arch_basic.sh
    Remove swap to use zramd.

--- arch_basic.sh
+++ arch_basic.sh
@@ -1,75 +1,105 @@
 #!/usr/bin/env sh
 
+############################################################
+#
+# 			Arch basic set up!!
+#
+############################################################
+
 #-------------------------------------------------------------------------------
 #                                                                  Key variables
+
 HOST_NAME=""
 ROOT_PASSWD=""
 USER=""
 USER_PASSWD=""
+
 SETUP_URL="https://raw.githubusercontent.com/zplat/Arch/main/basic_home_setup.sh"
 LOCAL_RESPOSITORY=".local/repositories/Arch"
+
 #-------------------------------------------------------------------------------
 #                                                                  set time
+
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc --localtime
+
 #-------------------------------------------------------------------------------
 #                                                                  set language locale.  
-echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+
 echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
-echo "fr_FR.UTF-8 UTF-8" >> /etc/locale.gen
-echo "it_IT.UTF-8 UTF-8" >> /etc/locale.gen
 echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
-echo "ko_KR.UTF-8 UTF-8" >> /etc/locale.gen
-echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen  #Well over the top!
+echo "ko_KR.UTF-8 UTF-8" >> /etc/locale.gen  #Well over the top!
+
 #-------------------------------------------------------------------------------
 #                                                                  Update locale
+
 locale-gen
+
 #-------------------------------------------------------------------------------
 #                                                                  Configure locale.conf.
+
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
 #-------------------------------------------------------------------------------
 #                                                                  Set terminal fonts
+
 echo "FONT=ter-132n" >> /etc/vconsole.conf
 echo "FONT_MAP=8859-1" >> /etc/vconsole.conf
+
 #-------------------------------------------------------------------------------
 #                                                                  Network
+
 echo "$HOST_NAME" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1  ${HOST_NAME}.localdomain $HOST_NAME" >> /etc/hosts
+
 #-------------------------------------------------------------------------------
 #                                                                  set root password
+
 echo "root:${ROOT_PASSWD}" | chpasswd
+
 #-------------------------------------------------------------------------------
 #                                                                  install packages
+
 pacman -S --noconfirm grub efibootmgr reflector
 pacman -S --noconfirm networkmanager network-manager-applet dialog wpa_supplicant
-pacman -S --noconfirm base-devel linux-headers 
+pacman -S --noconfirm base-devel linux-headers pacman-contrib
 pacman -S --noconfirm xdg-user-dirs xdg-utils rustup
 pacman -S --noconfirm zsh zsh-completions
 pacman -S --noconfirm terminus-font
 pacman -S --noconfirm dosfstools os-prober udiskie ntfs-3g openssl
 pacman -S --noconfirm xf86-video-amdgpu amd-ucode
+
 #-------------------------------------------------------------------------------
 #                                                                  change shell bash to zsh
+
 chsh -s /bin/zsh
+
 #-------------------------------------------------------------------------------
 #                                                                  Update pacman repositories. multilib.
+
 sed -i 's/^#\[multilib\]/\[multilib]/' /etc/pacman.conf
 sed -i '/^\[multilib\]/ {n;s/^#Include =  \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/nirrorlist/}' /etc/pacman.conf
+
 #-------------------------------------------------------------------------------
 #                                                                  setup mkinitcpio for amd graphics processor. 
-sed -i 's/MODULE.*/MODULE=\(amdgpu\)/' /etc/mkinitcpio.conf 
+
+sed -i 's/MODULE.*/MODULE=\(btrfs amdgpu\)/' /etc/mkinitcpio.conf 
 mkinitcpio -p linux
+
 #-------------------------------------------------------------------------------
 #                                                                  Install grub
+
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+
 #-------------------------------------------------------------------------------
 #                                                                  Setup systemctl hooks 
+
 #------------------------set reflector defaults
+
 echo "# Set the output path where the mirrorlist will be saved (--save).
 --save /etc/pacman.d/mirrorlist
 # Select the transfer protocol (--protocol).
@@ -85,6 +115,7 @@ echo "# Set the output path where the mirrorlist will be saved (--save).
 " > /etc/xdg/reflector/reflector.conf
 
 #------------------------Automate package cleaning with paccache
+
 echo "[Trigger]
 Operation = Remove
 Operation = Install
@@ -98,6 +129,7 @@ Exec = /usr/bin/paccache -rvk2
 " > /usr/share/libalpm/hooks/paccache.hook
 
 #------------------------list of orphan apps to remove
+
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -109,6 +141,7 @@ Exec = /usr/bin/bash -c "/usr/bin/pacman -Qtd || /usr/bin/echo '==> no orphans f
 " > /usr/share/libalpm/hooks/pkgClean.hook
 
 #------------------------list of Core programs
+
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -120,6 +153,7 @@ Exec = /bin/sh -c '/usr/bin/pacman -Qqn > /home/$USER/$LOCAL_RESPOSITORY/corepkg
 " > /usr/share/libalpm/hooks/pkgCore.hook
 
 #------------------------list of AUR programs
+
 echo "[Trigger]
 Operation = Install
 Operation = Remove
@@ -131,6 +165,7 @@ Exec = /bin/sh -c '/usr/bin/pacman -Qqm > /home/$USER/$LOCAL_RESPOSITORY/aurpkgl
 " > /usr/share/libalpm/hooks/pkgAUR.hook
 
 #------------------------Trigger udiskie on boot
+
 echo "[Unit]
 Description = Handle automounting of usb devices
 
@@ -144,49 +179,35 @@ WantedBy = multi-user.target
 
 #-------------------------------------------------------------------------------
 #                                                                  Systemd to start udiskie on startup
+
 chmod 755 /etc/systemd/user/udiskie.service
 
 #-------------------------------------------------------------------------------
-#                                                                  Update systemd to ensure dropbox starts on login
-systemctl enable dropbox@$USER
-#-------------------------------------------------------------------------------
 #                                                                  cacche
+
 pacman -s --noconfirm --needed ccache
 sed -i 'x;/^BUILDENV/s/!ccache/ccache/' /etc/makepkg.conf
 CORES=$(nproc)
 LOAD=$((CORES/2))
 JOBS=$((LOAD+1))
 sed -i "x;/^#MAKEFLAGS/s/"-j2"/-j${JOBS} -l${LOAD}/ " /etc/makepkg.conf
-#-------------------------------------------------------------------------------
-#                                                                  setup swap
-truncate -s 0 /swap/swapfile
-chattr +c /swap/swapfile
-btrfs property set /swap/swapfile compression none
-dd if=/dev/zero of=/swap/swapfile bs=1g count=2 status=progress
-chmod 600 /swap/swapfile
-mkswap /swap/swapfile
-swapon /swap/swapfile
-
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#                                                                  update fstab with swapfile
-echo "# swapfile" >> /etc/fstab
-echo "/swap/swapfile	none	swap	defaults	0 0" >> /etc/fstab
 
 #-------------------------------------------------------------------------------
 #                                                                  add user
+
 useradd -m -g users -s /bin/zsh "$user"
 echo "${user}:${user_passwd}" | chpasswd
 curl --url "$setup_url" >> "/home/$user/shell.sh"
 
 #-------------------------------------------------------------------------------
 #                                                                  make user an administrator
+
 echo "$user all=(all) all" >> "/etc/sudoers.d/$user"
 
 #-------------------------------------------------------------------------------
 #                                                                  enable systemd services
-systemctl enable networkmanager
+
+systemctl enable NetworkManager.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable paccache.timer
@@ -200,7 +221,8 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 #-------------------------------------------------------------------------------
 #                                                                  Install post install script.
-curl --url "$SETUP_URL" >> "/home/$USER/hell.sh"
+
+curl --url "$SETUP_URL" >> "/home/$USER/shell.sh"
 
 #-------------------------------------------------------------------------------
 #                                                                  all done 
