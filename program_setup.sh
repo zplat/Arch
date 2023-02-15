f1ccc9b171da1568bd8fea5f3fe4883555a4dc79
zplat <5zero.6cool@gmail.com>
2023-02-15 23:02:01 +0000

    Made the install list easier to read

--- program_setup.sh
+++ program_setup.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env sh
 
-############################################################
-#
-# 			Program set up!!
-#
-############################################################
-
 #-----------------------Install repositories
 
 mkdir ~/.local/repositories
 cd ~/.local/repositories/
 
-git clone https://aur.archlinux.org/paru.git        # Feature packed AUR helper
 git clone https://github.com/zplat/Arch.git         # My Arch Installation
-git clone https://github.com/zplat/neovim.git      # neovim (see Chris@machine, YTube)
-git clone https://github.com/zplat/qmk_firmware.git # cloned version of qmk: ferris9 layout
+git clone https://aur.archlinux.org/paru.git        # Feature packed AUR helper
 cd paru                                             # Install paru
 rustup default stable
 makepkg -si
@@ -27,25 +19,36 @@ if [ -s corepkglist.txt ]; then
 	sudo pacman -S --needed - <~/.local/repositories/corepkglist.txt
 else
 	# The file is empty.
-	sudo pacman --needed --noconfirm -S imv mpv feh sxiv
-	sudo pacman --needed --noconfirm -S nodejs python-gpgme
-	sudo pacman --needed --noconfirm -S wireplumber pipewire
-	sudo pacman --needed --noconfirm -S xorg-server xorg-apps alsa-utils python-pillow python-pygments
-	sudo pacman --needed --noconfirm -S tmux bat fzf broot fd ripgrep tmuxp xclip gawk glow
-	sudo pacman --needed --noconfirm -S graphviz zk calibre nyxt/surfraw
-	sudo pacman --needed --noconfirm -S picom fcitx5-mozc xbindkeys xorg-xinit fcitx5-config-qt
-	sudo pacman --needed --noconfirm -S sway swayidle swaylock foot swaybg
-	sudo pacman --needed --noconfirm -S xdg-desktop-portal-wlr
-	sudo pacman --needed --noconfirm -S bemenu-wayland bemenu rofi-pass
-	sudo pacman --needed --noconfirm -S surfraw notmuch isync msmtp msmtp-mta neomutt
-	sudo pacman --needed --noconfirm -S r tk rustup cmake
-	sudo pacman --needed --noconfirm -S git python-pip libffi gnupg ninja
-	sudo pacman --needed --noconfirm -S fcitx5 fcitx5-configtool fcitx5-mozc fcitx5-qt
-	sudo pacman --needed --noconfirm -S ttf-nerd-fonts-symbols-common
-	sudo pacman --needed --noconfirm -S ttf-nerd-fonts-symbols-1000-em ttf-nerd-fonts-symbols-1000-em-mono
-	sudo pacman --needed --noconfirm -S ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-2048-em-mono
-	sudo pacman --needed --noconfirm -S adobe-source-code-pro-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts
-#fi
+	# Keyboard layout and Fonts
+	sudo pacman --needed --noconfirm ttf-hack-nerd ttf-inconsolata-nerd ttf-sourcecodepro-nerd fcitx5 fcitx5-configtool fcitx5-mozc fcitx5-qt adobe-source-code-pro-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts
+	# Web Browsers and supporting applications
+	sudo pacman --needed --noconfirm -S nyxt surfraw
+	# Email
+	sudo pacman --needed --noconfirm -S notmuch isync msmtp msmtp-mta neomutt
+	# Image Viewers and Graphics
+	sudo pacman --needed --noconfirm -S imv feh sxiv graphviz
+	# Video Player
+	sudo pacman --needed --noconfirm -S mpv 
+	# pdf Readers
+	sudo pacman --needed --noconfirm -S calibre 
+	# Note takers
+	sudo pacman --needed --noconfirm -S zk 
+	# Audio
+	sudo pacman --needed --noconfirm -S wireplumber pipewire alsa-utils 
+	# X11 and Wayland
+	sudo pacman --needed --noconfirm -S xorg-server xbindkeys xorg-apps xorg-xinit picom xdg-desktop-portal-wlr  swayidle swaylock foot swaybg
+	# Terminals
+	sudo pacman --needed --noconfirm -S foot xterm alacritty
+	# Window Managers
+	sudo pacman --needed --noconfirm -S sway 
+	# Tools
+	sudo pacman --needed --noconfirm -S bemenu-wayland bemenu rofi-pass tmux bat fzf broot fd ripgrep tmuxp xclip gnupg 
+	# Programming
+	sudo pacman --needed --noconfirm -S r tk rustup cmake gawk 
+	# Required 
+	sudo pacman --needed --noconfirm -S python-pillow python-pygments python-gpgme python-pip nodejs libffi ninja
+	
+fi
 
 #-----------------------Install AUR packages
 
@@ -54,12 +57,21 @@ if [ -s aurpkglist.txt ]; then
 	paru -S --needed - <~/.local/repositories/aurpkglist.txt
 else
 	# The file is empty.
-	paru -S alacritty-git awesome-git river-git
-	paru -S vieb-bin-git buku-git lynx-current newsboat-git
+	## Keyboard layout and Fonts
+	paru -S ttf-envy-code-r qmk-git
+	# Web Browsers and supporting applications
+	paru -S google-chrome microsoft-edge-stable-bin vieb-bin-git buku-git lynx-current newsboat-git
+	# pdf Readers
+	paru -S zramd wlr-randr-git zathura-pdf-mupdf-git 
+	#Storage
+	paru -S dropbox 
+	# X11 and Wayland
+	paru -S wlr-randr-git zramd 
+	# Window Managers
+	paru -S awesome-git river-git
+	# Tools
 	paru -S rofi-lbonn-wayland-git
-	paru -S dropbox qmk-git google-chrome microsoft-edge-stable-bin
-	paru -S zramd wlr-randr-git zathura-pdf-mupdf-git
-	paru -S ttf-envy-code-r
+	
 fi
 
 #----------------------zramd systemd start and enable
