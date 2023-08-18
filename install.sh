ffcecbd8c16814026d8686dcc52978788a712a98
zplat <5zero.6cool@gmail.com>
2023-08-18 21:17:19 +0100

    Wip Updated

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 #Access additional partitions
 udiskie &
-
+Sleep 10
 #Where to store my downloaded repositories
 LOCAL_RESPOSITORY="/home/$USER/.local/repositories"
 
@@ -17,67 +17,22 @@ USEREMAIL=""
 USERNAME="zplat"
 
 #-----------------------Install Packages
- sudo pacman --needed --noconfirm -S ttf-hack ttf-hack-nerd ttf-inconsolata ttf-inconsolata-nerd ttf-sourcecodepro-nerd sudo pacman --needed --noconfirm -S pass 
+Install_Basic ()
+{
+ sudo pacman --needed --noconfirm -S ttf-hack ttf-hack-nerd ttf-inconsolata ttf-inconsolata-nerd ttf-sourcecodepro-nerd 
+ sudo pacman --needed --noconfirm -S pass 
  sudo pacman --needed --noconfirm -S xbindkeys xdg-desktop-portal-wlr nix qt5-wayland xorg-xwayland
  sudo pacman --needed --noconfirm -S foot alacritty
  sudo pacman --needed --noconfirm -S cmake gnupg 
  sudo pacman --needed --noconfirm -S rustup gawk 
+}
 
- rustup default nightly
-
-#-----------------------Setup .gnupg
-
-
-git config --global user.email "$USEREMAIL"
-git config --global user.name "$USERNAME"
-
-
-DIR="/run/media/phlight/Storage2/OnHold"
-DIR2="/run/media/phlight/Storage2/OnHold/.local/share"
-
-cp -pr "$DIR2/gnupg" ~/.local/share
-
-chown -R $(whoami) "${HOME}/.local/share/gnupg/"
-chmod 600 ~/.local/share/gnupg/*
-chmod 700 ~/.local/share/gnupg
-
-#-----------------------Setup .ssh
-
-cp -pr "$DIR/.ssh" ~/
-
-chown -R $(whoami) ~/.ssh
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/*
-
-#Start ssh and add password
-eval $(ssh-agent) #Start agent
-ssh-add "$HOME/.ssh/id_ed25519"
-
-#-----------------------Install repositories
-
-mkdir -p "$LOCAL_RESPOSITORY"
-cd "$LOCAL_RESPOSITORY"
-
-git clone git@github.com:zplat/Arch.git  # My Arch Installation
-
-git clone https://aur.archlinux.org/paru.git # Feature packed AUR helper
-
-#-----------------------Install paru
-cd paru # Install paru
-rustup default nightly
-makepkg -si
-
-cd # Return to home directory
-
-#-------------------------- Install dotfiles
-DOTFILES="git@github.com:zplat/MyDotfiles.git"
-
-git clone --bare --recurse-submodules "$DOTFILES" "$HOME/.dotfiles"
-
+#-----------------------Install Dotfiles
 function config {
 	/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-
+Config_files ()
+{
 mkdir -p "$HOME/.config-backup"
 config checkout
 
@@ -91,10 +46,11 @@ fi;
 config checkout -f
 config submodule update --init --recursive
 config config status.showUntrackedFiles no
-
+}
 
 #-----------------------Install AUR packages
-
+Install_AUR ()
+{
 ## Keyboard layout and Fonts
 paru -S ttf-envy-code-r 
 paru -S ttf-google-fonts-git
@@ -104,7 +60,6 @@ paru -S google-chrome
 # pdf Readers
 paru -S zathura-pdf-mupdf-git
 # System tools
-paru -S zramd
 paru -S qmk-git
 paru -S netrc
 #Storage
@@ -113,9 +68,10 @@ paru -S dropbox
 paru -S wlr-randr-git
 # Tools
 paru -S rofi-lbonn-wayland-git pass-tessen tessen
-
+}
 #-----------------------Setup password manager
-
+Install_Pass ()
+{
 git clone git@github.com:zplat/password-store.git ~/.password-store
 
 # Backup info for netrc set up with pass for password access.
@@ -124,9 +80,11 @@ git clone git@github.com:zplat/password-store.git ~/.password-store
 #curl -o "$NETRC-EXEC" "$GIT-CREDENTIAL-NETRC"
 
 git config --global credential.helper "netrc -f $HOME/.password-store/service.gpg -v -d"
-
+}
 
 #-----------------------Install More Packages
+Install_Core ()
+{
 sudo pacman -Syu
 # Web Browsers and supporting applications
 sudo pacman --needed --noconfirm -S nyxt surfraw fuzzel
@@ -155,9 +113,11 @@ sudo pacman --needed --noconfirm -S bemenu-wayland bemenu cmake rofi-pass tmux b
 sudo pacman --needed --noconfirm -S r tk rustup gawk 
 # Required 
 sudo pacman --needed --noconfirm -S python-pillow stfl python-pygments python-gpgme python-pip nodejs libffi ninja librsync python-pyserial
+}
 
 #-----------------------Install repositories
-
+Clone_Repositories ()
+{
 cd "$LOCAL_RESPOSITORY"
 
 git clone https://github.com/neovim/neovim.git      # forked (ufo-fold)
@@ -166,18 +126,25 @@ git clone https://github.com/newsboat/newsboat.git  # clone
 git clone https://github.com/qmk/qmk_firmware.git   # forked (ferris9)
 
 git clone https://github.com/kovidgoyal/kitty       # forked
-
+}
 
 #--------------------------nixos
+Setup_Nixos ()
+{
 sudo systemctl enable --now nix-daemon.service
 sudo gpasswd -a "$USER" nix-users
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --update
 #Install emanote
 nix-env -iA nixpkgs.emanote
+}
 
 #--------------------------zramd systemd start and enable
+Install_Swap ()
+{
+paru -S zramd
 sudo systemctl enable --now zramd.service
+}
 
 #--------------------------dropbox systemd start and enable
 #systemctl --user enable dropbox
